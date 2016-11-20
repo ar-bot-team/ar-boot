0a7a25a1176081fde4bfc3d10c1cc31eea0012bd
Ronald Q <ronald.otn@gmail.com>
2016-11-20 10:18:44 -0400

    add gulp and jshint and test

---
+++ Gulpfile.js
@@ -0,0 +1,34 @@
+// Include gulp
+var gulp = require('gulp');
+
+// Include Our Plugins
+var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
+var mocha = require('gulp-mocha');
+
+// Lint Task
+gulp.task('lint', function() {
+    return gulp.src([
+        './**/bin/*.js', './**/server/**/*.js'
+    ])
+        .pipe(jshint())
+        .pipe(jshint.reporter(stylish))
+        .pipe(jshint.reporter('fail'));
+});
+
+gulp.task('test', function () {
+    return gulp.src([
+        './arbot/test/*.js', './arbot-time/test/*.js', './arbot-weather/test/*.js'
+    ], { read: false })
+        .pipe(mocha({
+            reporter: 'spec'
+        }));
+});
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+    gulp.watch('js/*.js', ['lint', 'scripts']);
+});
+
+// Default Task
+gulp.task('default', ['lint', 'watch']);
