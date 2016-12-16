3b6ce18f09e4a025ee7a1a5b795fc877c68e205f
Rodrigo Quelca <rodrigo@processmaker.com>
2016-12-16 09:35:52 -0400

    add files

---
+++ libs/assets/listTpl.js
@@ -0,0 +1,46 @@
+const userList = require('./userList').userList;
+const _ = require('lodash');
+
+var elem = [],
+    user,
+    i,
+    users = _.get(userList, 'data');
+console.log(users);
+for (i = 0; i < users.length; i += 1){
+    user = users[i];
+    console.log(user);
+    elem.push({
+            "title": user.attributes.firstname + ' ' + user.attributes.lastname ,
+            "image_url": "https://www.processmaker.com/sites/all/themes/pmthemev2/img/white-badge.png",
+            "subtitle": user.attributes.email,
+            "buttons": [
+                {
+                    "title": "View Details",
+                    "type": "web_url",
+                    "url": "https://processmaker.com",
+                    //"messenger_extensions": true,
+                    "webview_height_ratio": "tall"
+                    //"fallback_url": "https://processmaker.com"
+                }
+            ]
+        });
+}
+
+console.log(userList);
+exports.listTpl = {
+    "attachment": {
+        "type": "template",
+        "payload": {
+            "template_type": "list",
+            "elements": elem,
+
+            "buttons": [
+                {
+                    "title": "View More",
+                    "type": "postback",
+                    "payload": "payload"
+                }
+            ]
+        }
+    }
+};
